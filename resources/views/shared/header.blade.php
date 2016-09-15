7b7168f8f88c89b6abb31caef95e45b624cf7391
ari_fakhri <arifakhri97@gmail.com>
2016-09-15 12:51:19 +0700

    make folder shared for header

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="nav-wrapper">        
+  <ul class="left hide-on-med-and-down">
+    <li><img src="img/example_logo.gif" width="150px" height="60px"></li>
+  </ul>
+  <a href="#" data-activates="side-nav-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+  <ul class="right hide-on-med-and-down"> 
+    <li><a href="/">Officers List<i class="material-icons right">view_list</i></a></li>
+    <li> &nbsp;</li>
+  </ul>
+  <ul class="side-nav" id="side-nav-demo">
+    <li><a href="/">Officer List</a></li>
+  </ul>
+    </div>
+</nav>
