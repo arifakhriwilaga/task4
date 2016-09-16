8ffaf3009f3865cc73a5e5f24232c4023352bc06
ari_fakhri <arifakhri97@gmail.com>
2016-09-16 08:17:04 +0700

    add notif error

--- resources/views/officer/officer_create.blade.php
+++ resources/views/officer/officer_create.blade.php
@@ -10,10 +10,16 @@
 {!! Form::open(['route' => 'officer_store']) !!}
 <!-- Input name -->
 {!! Form::label('name', 'Name') !!}
-{!! Form::text('input_name',null) !!}
+{!! Form::text('input_name') !!}
+{!! $errors->first('input_name') !!}
+
+<br>
+<br>
+<br>
 <!-- Input address -->
 {!! Form::label('address', 'Address') !!}
-{!! Form::text('input_address',null) !!}
+{!! Form::text('input_address') !!}
+{!! $errors->first('input_address') !!}  
 
 {!! Form::submit('Add Officer!') !!}
 {!! Form::close() !!}
