b13bb2e3e5d5d24e69e97cff3ba09d36f870a016
ari_fakhri <arifakhri97@gmail.com>
2016-09-25 06:06:21 +0700

    -make view for import article and comment

---
+++ resources/views/excel/import_images.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.layout')
+
+@section('content')
+  <br>
+  <p>
+
+<div class="col-xs-4">
+</div>
+<!-- Form Open -->
+
+<div class="col-xs-6">
+<div class="well">
+
+{!! Form::open(['route' => 'article_store', 'enctype' => 'multipart/form-data']) !!}
+<h2>Post your image</h2>
+<!-- Input User ID -->
+    <div class="form-group">
+        <input hidden="" name="input_user_id" value="{{ Sentinel::getUser()->id }}">
+    </div>
+{!! $errors->first('input_user_id') !!} 
+
+<!-- Input title image -->
+    <div class="form-group">
+        <input class="form-control" placeholder="Title Image" type="text" name="input_title_image">
+    </div>
+{!! $errors->first('input_title_image') !!} 
+
+<!-- Input description image -->
+    <div class="form-group">
+        <input class="form-control" placeholder="Description Image" type="text" name="input_description_image">	
+    </div>
+{!! $errors->first('input_description_image') !!}  
+
+{!! Form::file('image',null) !!}
+{!! $errors->first('image') !!} 
+
+<br><br><br>
+{!! Form::submit('Save') !!}
+{!! Form::close() !!}
+</div>
+<!-- close form -->
+
+</div>
+<div class="col-xs-4">
+</div>
+@stop
